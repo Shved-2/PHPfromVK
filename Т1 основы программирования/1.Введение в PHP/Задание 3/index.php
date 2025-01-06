4b1300cae9004cfc5c93284dcdacefc2c11e216c
Ilia Shved <ilia83ivanovich@gmail.com>
2025-01-06 21:48:39 +0500

    vvedenie

---
+++ Т1 основы программирования/1.Введение в PHP/Задание 3/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Программирование на языке PHP</title>
+</head>
+<body>
+	
+	<h1>Основы программирования</h1>
+	<h2>Введение в PHP</h2>
+	<hr>
+	<h2>Все будет правильно, на этом построен мир</h2>
+
+	<?php
+
+		echo '<p>But it wasn’t good either.</p>';
+
+		echo '<p><b>“This sheep is too old. I want a sheep which will live a long time.”</b></p>';
+
+		echo '<p>At that moment, my patience was gone, because I was in a hurry to start repairing my engine, I quickly drew this picture.</p>';
+
+		echo '<p>And I said, <b>“That’s a box. The sheep which you want is inside.”</b></p>';
+
+		echo '<p>I was very surprised when I saw the light in the face of the young man, <b>“This is exactly how I wanted it! Do you think that the sheep will need a lot of grass?”</b></p>';
+
+		echo '<p><b>“Why?”</b></p>';
+
+		echo '<p><b>“Because where I live, everything is very small.”</b></p>';
+
+		echo '<p><b>“I believe that there will be enough grass for it. I gave you a very small sheep.”</b></p>';
+
+		echo '<p>He looked closely at the picture, <b>“It is not very small. Look! It is sleeping now.”</b></p>';
+
+		echo '<p> And this is how I met the little prince.</p>';
+
+	?>
+	<footer align="center">
+		<h3>Веб-разработка | Профессионалы | Демоэкзамен</h3>
+		<a href="https://vk.com/pechora_pro" target="_blank">pechora_PRO</a>
+	</footer>
+</body>
+</html>
